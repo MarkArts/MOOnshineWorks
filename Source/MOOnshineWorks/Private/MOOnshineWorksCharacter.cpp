15c603699c3061aa251959ccb5ae6e51b4db4019
Randy Dijkstra <randydijkstra92@gmail.com>
2014-11-07 13:30:14 +0100

    Minor stuff.. added map to test sensing ai

--- Source/MOOnshineWorks/Private/MOOnshineWorksCharacter.cpp
+++ Source/MOOnshineWorks/Private/MOOnshineWorksCharacter.cpp
@@ -115,7 +115,7 @@ void AMOOnshineWorksCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVect
 void AMOOnshineWorksCharacter::StartAim()
 {
     CameraBoom->TargetArmLength = 175.0f;
-    CameraBoom->SocketOffset = FVector(0.0f, 50.0f, 0.0f);
+    CameraBoom->SocketOffset = FVector(10.0f, 60.0f, 0.0f);
 }
 
 void AMOOnshineWorksCharacter::EndAim()
@@ -167,22 +167,12 @@ void AMOOnshineWorksCharacter::MoveRight(float Value)
 
 void AMOOnshineWorksCharacter::StartSprint()
 {
-    //CharacterMovement->MaxWalkSpeed = 2000.0f;
-    //CharacterMovement->MaxWalkSpeed = (SpeedFactor * PowerLevel + BaseSpeed)*2;
-    // CameraBoom->TargetArmLength = 175.0f;
-    // CameraBoom->SocketOffset = FVector(0.0f, 50.0f, 0.0f);
-    
     CharacterMovement->MaxWalkSpeed *= 1.5;
     IsSprinting = true;
 }
 
 void AMOOnshineWorksCharacter::EndSprint()
 {
-    //CharacterMovement->MaxWalkSpeed = 1000.0f;
-    //CharacterMovement->MaxWalkSpeed = (SpeedFactor * PowerLevel + BaseSpeed)/2;
-    //  CameraBoom->TargetArmLength = 150.0f;
-    //  CameraBoom->SocketOffset = FVector(0.0f, 50.0f, 50.0f);
-    
     CharacterMovement->MaxWalkSpeed /= 3;
     CharacterMovement->MaxWalkSpeed *= 2;
     IsSprinting = false;
