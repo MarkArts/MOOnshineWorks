cd615946dd057d289704a6fe3bd74a0c93cb5e7c
Dennis Alioglou <den_ali9@hotmail.com>
2014-11-20 15:11:22 +0100

    Code goed gezet met merge

--- Source/MOOnshineWorks/Private/BlueprintLoader.cpp
+++ Source/MOOnshineWorks/Private/BlueprintLoader.cpp
@@ -8,10 +8,10 @@
 
 BlueprintLoader::BlueprintLoader()
 {
-	AddBP(FName("BP_Projectile"), TEXT("/Game/Blueprints/BP_Projectile"));
-	AddBP(FName("AI_BarrelEnemy"), TEXT("/Game/Blueprints/AIBlueprints/BarrelEnemy/Services/AI_BarrelEnemy"));
-	AddBP(FName("AI_BookEnemyLight"), TEXT("/Game/Blueprints/AIBlueprints/BookLight/AI_BookEnemyLight"));
-	AddBP(FName("MyCharacter"), TEXT("/Game/Blueprints/MyCharacter"));
+	AddBP(FName("BP_Projectile"), ANSI_TO_TCHAR("/Game/Blueprints/BP_Projectile"));
+	AddBP(FName("AI_BarrelEnemy"), ANSI_TO_TCHAR("/Game/Blueprints/AIBlueprints/BarrelEnemy/Services/AI_BarrelEnemy"));
+	AddBP(FName("AI_BookEnemyLight"), ANSI_TO_TCHAR("/Game/Blueprints/AIBlueprints/BookLight/AI_BookEnemyLight"));
+	AddBP(FName("MyCharacter"), ANSI_TO_TCHAR("/Game/Blueprints/MyCharacter"));
 }
 
 BlueprintLoader::~BlueprintLoader()
