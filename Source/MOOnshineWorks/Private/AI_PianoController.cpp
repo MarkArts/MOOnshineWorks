a522b43ed92f2e954c34087afc034acb478a8463
Falko Neven <falko2010@live.nl>
2014-12-09 13:06:09 +0100

    PianoUpdate

---
+++ Source/MOOnshineWorks/Private/AI_PianoController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MOOnshineWorks.h"
+#include "AI_PianoController.h"
+
+
+AAI_PianoController::AAI_PianoController(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+void AAI_PianoController::AttackPlayer()
+{
+
+}
+void AAI_PianoController::SpeedUp()
+{
+
+}
+void AAI_PianoController::PianoPatrol()
+{
+
+}
+
