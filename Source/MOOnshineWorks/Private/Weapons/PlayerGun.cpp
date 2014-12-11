092defa045360cd3b77f823cc6091bf7b0d52635
Randy Dijkstra <randydijkstra92@gmail.com>
2014-12-11 13:06:54 +0100

    Updated HUD with latest ammo function

--- Source/MOOnshineWorks/Private/Weapons/PlayerGun.cpp
+++ Source/MOOnshineWorks/Private/Weapons/PlayerGun.cpp
@@ -89,6 +89,7 @@ void APlayerGun::OnInteract_Implementation(AActor* Target)
 int32 APlayerGun::GetRemainingShotCount()
 {
 	int32 Result = AmmoContainer->GetAmmo(FindActiveAmmoType());
+//    Result = FPlatformMath::FloorToInt(Result / FindActiveMultiplier());
 	Result = FMath::Floor(Result / FindActiveMultiplier());
 	return Result;
 }
