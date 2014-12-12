67bb2633f7f0d8c90019b0b0be8c6e481b695e8b
Mark Arts <105424@live.nl>
2014-12-12 11:11:07 +0100

    Added setup for changeable

---
+++ Source/MOOnshineWorks/Public/Changeable.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Changeable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONSHINEWORKS_API AChangeable : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MOO)
+	TSubclassOf<APawn> TargetClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MOO)
+	UBehaviorTree* BehaviorTree;
+
+	UFUNCTION(BlueprintNativeEvent, Category = MOO)
+	void Activate();
+
+	UFUNCTION(BlueprintCallable, Category = MOO)
+	void Change();
+
+	
+};
