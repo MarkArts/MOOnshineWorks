7cf77b314cd9d6c407819772650a6f618e4f5aec
Falko Neven <falko2010@live.nl>
2015-01-14 14:55:15 +0100

    Lamp added, fix movementspeed Peg->Active

---
+++ Source/MOOnshineWorks/Public/AI/Runner/Lamp/AI_LampEnemy.h
@@ -0,0 +1,18 @@
+// Creative Common Share Alike
+
+#pragma once
+
+#include "AI_RunnerEnemy.h"
+#include "AI_BasicEnemy.h"
+#include "AI_LampEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONSHINEWORKS_API AAI_LampEnemy : public AAI_RunnerEnemy
+{
+	GENERATED_BODY()
+public:
+	AAI_LampEnemy(const class FObjectInitializer& PCIP);
+};
