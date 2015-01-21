efcf8516fe49d626f612ae98b8e4bcacf57f04f9
PrPleGoo <felix.leeuwen@gmail.com>
2015-01-21 10:12:52 +0100

    fixed the didhit bool not being set when a projectile hits an actor

--- Source/MOOnshineWorks/Private/Weapons/Projectile.cpp
+++ Source/MOOnshineWorks/Private/Weapons/Projectile.cpp
@@ -60,7 +60,7 @@ void AProjectile::OnHit(AActor* OtherActor, UPrimitiveComponent* OtherComp, FVec
 		{
 			if (OtherActor != GetOwner()->GetOwner())
 			{
-				if (OtherActor->GetClass()->IsChildOf(APlayerGun::StaticClass()))
+				if (GetOwner()->GetClass()->IsChildOf(APlayerGun::StaticClass()))
 				{
 					APlayerGun* PlayerGun = Cast<APlayerGun>(GetOwner());
 					if (PlayerGun)
