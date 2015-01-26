721b0c4699474c75a13b4c9c5d1a7893f2436a33
Mark Arts <105424@live.nl>
2015-01-26 16:15:05 +0100

    Interactable gun now ovverides interact isntead of interact implementation

--- Source/MOOnshineWorks/Private/InteractableGun.cpp
+++ Source/MOOnshineWorks/Private/InteractableGun.cpp
@@ -11,22 +11,29 @@ AInteractableGun::AInteractableGun(const class FObjectInitializer& PCIP)
 	AmmoGivenIfHasGun = 5;
 }
 
-void AInteractableGun::OnInteract_Implementation(AActor* Target)
+void AInteractableGun::Interact(AActor* Target)
 {
-	AMOOnshineWorksCharacter* CharTarget = Cast<AMOOnshineWorksCharacter>(Target);
-	if (CharTarget)
+	if (ShouldUseOnce)
 	{
-		APlayerGun* NewGun = GetWorld()->SpawnActor<APlayerGun>(Gun);
-		if (!CharTarget->WeaponStrap->ContainsGun(Gun) && NewGun)
-		{
-			CharTarget->EquipGun(NewGun);
-		}
-		else if (CharTarget->WeaponStrap->ContainsGun(Gun) && NewGun)
-		{
-			CharTarget->AmmoContainer->AddAmmo(NewGun->AmmoTypes[NewGun->ActiveIndex], AmmoGivenIfHasGun);
-			UsedText = FString(TEXT("Already have this weapon, picked up the ammo instead"));
-			NewGun->Destroy();
+		if (!IsUsed){
+			AMOOnshineWorksCharacter* CharTarget = Cast<AMOOnshineWorksCharacter>(Target);
+			if (CharTarget)
+			{
+				APlayerGun* NewGun = GetWorld()->SpawnActor<APlayerGun>(Gun);
+				if (!CharTarget->WeaponStrap->ContainsGun(Gun) && NewGun)
+				{
+					CharTarget->EquipGun(NewGun);
+				}
+				else if (CharTarget->WeaponStrap->ContainsGun(Gun) && NewGun)
+				{
+					CharTarget->AmmoContainer->AddAmmo(NewGun->AmmoTypes[NewGun->ActiveIndex], AmmoGivenIfHasGun);
+					UsedText = FString(TEXT("Already have this weapon, picked up the ammo instead"));
+					NewGun->Destroy();
+				}
+			}
+			Destroy();
 		}
 	}
-	Destroy();
+
+	Super::Interact(Target);
 }
