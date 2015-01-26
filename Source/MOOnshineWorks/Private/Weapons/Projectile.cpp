302025640def5ef311f855e32903dad1cb967776
PrPleGoo <PrPleGoo@users.noreply.github.com>
2015-01-26 11:46:12 +0100

    Update Projectile.cpp
    Removed some double code for DidHit

--- Source/MOOnshineWorks/Private/Weapons/Projectile.cpp
+++ Source/MOOnshineWorks/Private/Weapons/Projectile.cpp
@@ -123,12 +123,6 @@ void AProjectile::HitActor(AActor* OtherActor)
 		if (TargetEnemy)
 		{
 			TargetEnemy->DealDamage(DamageValue);
-            APlayerGun* PlayerGun = Cast<APlayerGun>(GetOwner());
-            if (PlayerGun)
-            {
-                AMOOnshineWorksCharacter* Player = Cast<AMOOnshineWorksCharacter>(PlayerGun->GetOwner());
-                Player->DidHit = true;
-            }
 		}
 	}
 	if (OtherActor->GetClass()->IsChildOf(AMOOnshineWorksCharacter::StaticClass()))
